712523b2c1a7a6f22a217b2e341e246bd3803414
Andre220 <andreprofissional1997@gmail.com>
2022-07-24 02:40:41 -0300

    Lessons until 106

--- Assets/Scripts/Networking/102/Player.cs
+++ Assets/Scripts/Networking/102/Player.cs
@@ -2,13 +2,32 @@ using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : NetworkBehaviour
 {
     private NetworkCharacterControllerPrototype _cc;
 
     [SerializeField] private Ball _prefabBall;
+    [SerializeField] private PhysxBall _prefabPhysxBall;
     private Vector3 _forward;
+
+    private Text _messages;
+
+    [Networked(OnChanged = nameof(OnBallSpawned))]
+    public NetworkBool spawned { get; set; }
+
+    private Material _material;
+
+    Material material 
+    {
+        get 
+        { 
+            if(_material == null) 
+                _material = GetComponentInChildren<MeshRenderer>().material; 
+            return _material; 
+        }
+    }
 
     [Networked] private TickTimer delay { get; set; }
 
@@ -16,6 +35,38 @@ public class Player : NetworkBehaviour
     {
         _cc = GetComponent<NetworkCharacterControllerPrototype>();
         _forward = transform.forward;
+
+        _messages = FindObjectOfType<Canvas>().gameObject.transform.Find("Chat").GetComponent<Text>();
+    }
+
+    public static void OnBallSpawned(Changed<Player> changed)
+    {
+        changed.Behaviour.material.color = Color.white;
+    }
+
+    public override void Render()
+    {
+        material.color = Color.Lerp(material.color, Color.blue, Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (Object.HasInputAuthority && Input.GetKeyDown(KeyCode.R))
+        {
+            RPC_SendMessage("Hey mate!");
+        }
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+    private void RPC_SendMessage(string message, RpcInfo info = default)
+    {
+        if (_messages == null)
+            _messages = FindObjectOfType<Text>();
+        if (info.IsInvokeLocal)
+            message = $"You said: {message}\n";
+        else
+            message = $"Some other player said: {message}\n";
+        _messages.text += message;
     }
 
     public override void FixedUpdateNetwork()
@@ -40,6 +91,21 @@ public class Player : NetworkBehaviour
                         // Initialize the Ball before synchronizing it
                         o.GetComponent<Ball>().Init();
                     });
+
+                    spawned = !spawned;
+                }
+                else if((data.buttons & NetworkInputData.MOUSEBUTTON2) != 0)
+                {
+                    delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
+                    Runner.Spawn(_prefabPhysxBall,
+                    transform.position + _forward, Quaternion.LookRotation(_forward),
+                    Object.InputAuthority, (runner, o) =>
+                    {
+                        // Initialize the Ball before synchronizing it
+                        o.GetComponent<PhysxBall>().Init(_forward * 10);
+                    });
+
+                    spawned = !spawned;
                 }
             }
         }
